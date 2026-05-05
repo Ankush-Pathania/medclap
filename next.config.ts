9e481f89250e608574c15fc9618768182fa6bfc4
Harish <harishk.medclap@gmail.com>
2026-05-05 16:50:10 +0530

    redirect to homepage /california /tennessee /new-york

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/california",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/tennessee",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/new-york",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
